9432c8d900486d2c98e3e45bf7f3549019cb4e45
2changjon <2changjon@naver.com>
2021-04-02 15:33:24 +0900

    문제 풀기

---
+++ src/Baekjoon/Array/Max_Value.java
@@ -0,0 +1,28 @@
+package Baekjoon.Array;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Max_Value {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int[] arr= new int[9];
+		int max =0;
+		int count = 0;
+		
+		for(int i =0; i<9; i++) {
+			String bf_s = bf.readLine();
+			arr[i] = Integer.parseInt(bf_s);
+			if(max < arr[i]) {
+				max = arr[i];
+				count = i+1;
+			}
+		
+		}
+		 bf.close();
+	
+		System.out.println(max);
+		System.out.print(count);
+	}
+}
---
+++ src/Baekjoon/Array/Min_Max_Find.java
@@ -0,0 +1,31 @@
+package Baekjoon.Array;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Min_Max_Find {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(bf.readLine());
+		String[] arr = bf.readLine().split(" ");
+		
+		int min;
+		int max;
+			
+		max = min = Integer.parseInt(arr[0]);
+		
+		for(int i = 1; i<num; i++) {
+			if (min > Integer.parseInt(arr[i])) {
+				min = Integer.parseInt(arr[i]); 
+			}else if(max < Integer.parseInt(arr[i])){
+				max = Integer.parseInt(arr[i]); 
+			}
+		}
+		
+		bf.close();
+		
+		System.out.println(min+" "+max);
+	}
+
+}
